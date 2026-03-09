b8018d33631a14c1da969197c1f85a33350a4bb3
Raunak Warrier <raunakwarrier@outlook.com>
2026-03-09 21:16:37 +0530

    fix typo errors

--- src/main/java/com/gitsandbox/service/GitService.java
+++ src/main/java/com/gitsandbox/service/GitService.java
@@ -45,7 +45,7 @@ public class GitService {
             return "Initialized empty Git Repository";
         }
         if (!isInitialized) {
-            return "Fatal: not a git repository";
+            return "fatal: not a git repository";
         }
 
         // Touch (create file)
